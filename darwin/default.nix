1e50a0216e60431c58e2492cccabbf7be5b7c86f
Andrii Nasinnyk <anasinnyk@macpaw.com>
2024-05-09 04:54:18 +0300

    fix skhdrc

--- darwin/default.nix
+++ darwin/default.nix
@@ -70,77 +70,7 @@
     };
     skhd = {
       enable = true;
-      skhdConfig = ''
-        # focus window
-        alt - h : yabai -m window --focus west
-        alt - j : yabai -m window --focus south
-        alt - k : yabai -m window --focus north
-        alt - l : yabai -m window --focus east
-
-        # swap managed window
-        shift + alt - h : yabai -m window --swap west
-        shift + alt - j : yabai -m window --swap south
-        shift + alt - k : yabai -m window --swap north
-        shift + alt - l : yabai -m window --swap east
-
-        # move managed window
-        shift + alt + ctrl - h : yabai -m window --warp west
-        shift + alt + ctrl - j : yabai -m window --warp south
-        shift + alt + ctrl - k : yabai -m window --warp north
-        shift + alt + ctrl - l : yabai -m window --warp east
-
-        # rotate tree
-        alt + shift - r : yabai -m space --rotate 90
-
-        # toggle window fullscreen zoom
-        alt + shift - f : yabai -m window --toggle zoom-fullscreen
-
-        # float / unfloat window and center on screen
-        alt + shift - t : yabai -m window --toggle float;\
-                  yabai -m window --grid 4:4:1:1:2:2
-
-        # toggle window split type
-        alt + shift - e : yabai -m window --toggle split
-
-        # move window and focus desktop
-        shift + alt - 1 : w=$(yabai -m query --windows --window | jq '.id'); yabai -m window --space 1; yabai -m window --focus $w
-        shift + alt - 2 : w=$(yabai -m query --windows --window | jq '.id'); yabai -m window --space 2; yabai -m window --focus $w
-        shift + alt - 3 : w=$(yabai -m query --windows --window | jq '.id'); yabai -m window --space 3; yabai -m window --focus $w
-        shift + alt - 4 : w=$(yabai -m query --windows --window | jq '.id'); yabai -m window --space 4; yabai -m window --focus $w
-        shift + alt - 5 : w=$(yabai -m query --windows --window | jq '.id'); yabai -m window --space 5; yabai -m window --focus $w
-        shift + alt - 6 : w=$(yabai -m query --windows --window | jq '.id'); yabai -m window --space 6; yabai -m window --focus $w
-        shift + alt - 7 : w=$(yabai -m query --windows --window | jq '.id'); yabai -m window --space 7; yabai -m window --focus $w
-        shift + alt - 8 : w=$(yabai -m query --windows --window | jq '.id'); yabai -m window --space 8; yabai -m window --focus $w
-        shift + alt - 9 : w=$(yabai -m query --windows --window | jq '.id'); yabai -m window --space 9; yabai -m window --focus $w
-        shift + alt - 0 : w=$(yabai -m query --windows --window | jq '.id'); yabai -m window --space 10; yabai -m window --focus $w
-
-        alt - 1 : w=$(yabai -m query --spaces | jq '.[] | select(.index == 1) | ."first-window"'); yabai -m window --focus $w
-        alt - 2 : w=$(yabai -m query --spaces | jq '.[] | select(.index == 2) | ."first-window"'); yabai -m window --focus $w
-        alt - 3 : w=$(yabai -m query --spaces | jq '.[] | select(.index == 3) | ."first-window"'); yabai -m window --focus $w
-        alt - 4 : w=$(yabai -m query --spaces | jq '.[] | select(.index == 4) | ."first-window"'); yabai -m window --focus $w
-        alt - 5 : w=$(yabai -m query --spaces | jq '.[] | select(.index == 5) | ."first-window"'); yabai -m window --focus $w
-        alt - 6 : w=$(yabai -m query --spaces | jq '.[] | select(.index == 6) | ."first-window"'); yabai -m window --focus $w
-        alt - 7 : w=$(yabai -m query --spaces | jq '.[] | select(.index == 7) | ."first-window"'); yabai -m window --focus $w
-        alt - 8 : w=$(yabai -m query --spaces | jq '.[] | select(.index == 8) | ."first-window"'); yabai -m window --focus $w
-        alt - 9 : w=$(yabai -m query --spaces | jq '.[] | select(.index == 9) | ."first-window"'); yabai -m window --focus $w
-        alt - 0 : w=$(yabai -m query --spaces | jq '.[] | select(.index == 10) | ."first-window"'); yabai -m window --focus $w
-
-        # fast prev focus
-        alt - tab : yabai -m window --focus recent
-
-        # send window to monitor and follow focus
-        shift + alt - n : w=$(yabai -m query --windows --window | jq '.id'); yabai -m window --display next; yabai -m window --focus $w
-        shift + alt - p : w=$(yabai -m query --windows --window | jq '.id'); yabai -m window --display prev; yabai -m window --focus $w
-        shift + alt - r : \
-          w=$(yabai -m query --windows --window | jq '.id'); \
-          cs=$(yabai -m query --spaces --space | jq '.index'); \
-          ns=$(yabai -m query --spaces | jq '[.[] | select(."is-visible" == true and ."has-focus" == false) | .index][0]'); \
-          csws=$(yabai -m query --spaces --space | jq '.windows | .[]'); \
-          nsws=$(yabai -m query --spaces --space $ns | jq '.windows | .[]'); \
-          echo $csws | xargs -n1 -I{} yabai -m window {} --space $ns; \
-          echo $nsws | xargs -n1 -I{} yabai -m window {} --space $cs; \
-          yabai -m window --focus $w;
-      '';
+      skhdConfig = builtins.readFile ./skhdrc;
     };
   };
 
