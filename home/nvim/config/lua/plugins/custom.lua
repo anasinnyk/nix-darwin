e9776b2cfce77dd24683b96dbf960ec937cc226f
Andrii Nasinnyk <anasinnyk@macpaw.com>
2024-05-09 18:20:04 +0300

    Normal and Visaual-line/block comment mode

--- home/nvim/config/lua/plugins/custom.lua
+++ home/nvim/config/lua/plugins/custom.lua
@@ -1,11 +1,26 @@
 return {
 	"tpope/vim-sleuth", -- check .editorconfig
-	{ "numToStr/Comment.nvim", opts = {} }, -- "gc" to comment visual regions/lines
+	{
+		"numToStr/Comment.nvim",
+		opts = {},
+		config = function()
+			vim.keymap.set("n", "<leader>/", function()
+				return vim.v.count == 0 and "<Plug>(comment_toggle_linewise_current)"
+					or "<Plug>(comment_toggle_linewise_count)"
+			end, { expr = true, desc = "Comment current code line(s)" })
+			vim.keymap.set("x", "<leader>/", function()
+				print(vim.fn.visualmode())
+				print(vim.fn.visualmode())
+				return vim.fn.visualmode() == "V"
+						and '<ESC><CMD>lua require("Comment.api").locked("toggle.linewise")(vim.fn.visualmode())<CR>'
+					or '<ESC><CMD>lua require("Comment.api").locked("toggle.blockwise")(vim.fn.visualmode())<CR>'
+			end, { expr = true, desc = "(Un)Comment current code line/block" })
+		end,
+	}, -- "gc" to comment visual regions/lines
 	{
 		"lukas-reineke/indent-blankline.nvim",
 		event = "BufReadPost",
 		main = "ibl",
-		opts = {
-		},
+		opts = {},
 	},
 }
