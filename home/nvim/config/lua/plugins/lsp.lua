151df242f2e51f0120c061ea796e605c670fdbb8
Andrii Nasinnyk <anasinnyk@macpaw.com>
2024-05-24 02:11:51 +0300

    fix lsp keybinding

--- home/nvim/config/lua/plugins/lsp.lua
+++ home/nvim/config/lua/plugins/lsp.lua
@@ -142,7 +142,10 @@ return {
 			lsp_zero.extend_lspconfig()
 
 			lsp_zero.on_attach(function(client, bufnr)
-				lsp_zero.default_keymaps({ buffer = bufnr })
+				lsp_zero.default_keymaps({
+					buffer = bufnr,
+					preserve_mappings = false,
+				})
 				lsp_zero.buffer_autoformat()
 			end)
 
